29c6c7b86fdea34ad82b6bb4fd0391023eebfb6f
prog-daiki <t.daiki0913@icloud.com>
2024-09-16 21:19:45 +0900

    FE: hono.tsを修正

--- frontend/lib/hono.ts
+++ frontend/lib/hono.ts
@@ -1,4 +1,4 @@
 import { hc } from "hono/client";
 import { type AppType } from "../../server/app";
 
-export const client = hc<AppType>("http://localhost:8000/");
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!);
